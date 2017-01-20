ab07334b1975eca4adc6b4a215349c517d58ebaa
pawel <pawel_s1@o2.pl>
2017-01-20 23:33:48 +0100

    Prototype of destroying

---
+++ Earthquake/Assets/Scripts/ClickDetector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickDetector : MonoBehaviour
+{
+
+   // Use this for initialization
+   void Start()
+   {
+
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      if (Input.GetMouseButton(0))
+      {
+         RaycastHit hit;
+         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+         if (Physics.Raycast(ray, out hit))
+         {
+            //hit.transform.GetComponent<Rigidbody>().AddExplosionForce(9999999, Vector3.up, 0, 0);
+            hit.transform.GetComponent<Rigidbody>().AddForce(Vector3.up*100, ForceMode.Impulse);
+         }
+      }
+   }
+}
---
+++ Earthquake/Assets/Scripts/Creator.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creator : MonoBehaviour
+{
+
+   public GameObject Origin;
+
+	// Use this for initialization
+	void Start ()
+	{
+	   Object objectToLoad = Resources.Load("Prefabs/PieceOfTerrain", typeof(GameObject));
+	   const int gridSize = 30;
+      var objects = new Dictionary<Position, GameObject>();
+	   for (int i = 0; i < gridSize; i++)
+	   {
+	      for (int j = 0; j < gridSize; j++)
+	      {
+            GameObject instance = Instantiate(objectToLoad, new Vector3(i, 0, j), Quaternion.identity) as GameObject;
+	         objects[new Position(i, j)] = instance;
+	      }
+	   }
+
+	   for (int i = 0; i < gridSize; i++)
+	   {
+	      for (int j = 0; j < gridSize; j++)
+	      {
+	         GameObject currentPiece = objects[new Position(i, j)];
+	         var currentMovable = currentPiece.GetComponent<PieceOfTerrain>().MovablePart;
+            var upperNeighbourPosition = new Position(i+1, j);
+	         JoinToNeighbour(objects, upperNeighbourPosition, currentMovable);
+            var rightNeighbourPosition = new Position(i, j+1);
+	         JoinToNeighbour(objects, rightNeighbourPosition, currentMovable);
+	      }
+	   }
+	   
+	}
+
+   private static void JoinToNeighbour(Dictionary<Position, GameObject> objects, Position neighbourPosition, GameObject currentMovable)
+   {
+      if (objects.ContainsKey(neighbourPosition))
+      {
+         var upperNeighbour = objects[neighbourPosition];
+         var upperNeighbourMovable = upperNeighbour.GetComponent<PieceOfTerrain>().MovablePart;
+
+         var joint = currentMovable.AddComponent<SpringJoint>();
+         joint.connectedBody = upperNeighbourMovable.GetComponent<Rigidbody>();
+         joint.spring = 50f;
+      }
+   }
+
+   // Update is called once per frame
+	void Update () {
+		
+	}
+}
