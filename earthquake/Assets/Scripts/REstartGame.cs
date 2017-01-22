c326ff84be1becb83e136b6eea176478ece62baa
Leonard Maxim <leonard.maxim@outlook.com>
2017-01-22 13:47:59 +0100

    Restart level button

---
+++ earthquake/Assets/Scripts/REstartGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class REstartGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void restartScene()
+    {
+        Application.LoadLevel("Level");
+    }
+}
